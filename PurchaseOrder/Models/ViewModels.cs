54eb1a90bbd6eeabf4f97f501f8bad02b5f665a5
leo <leoli.qd@hotmail.com>
2018-11-28 15:38:00 +0800

    錯誤更新

--- PurchaseOrder/Models/ViewModels.cs
+++ PurchaseOrder/Models/ViewModels.cs
@@ -6,6 +6,31 @@ using System.Web;
 
 namespace PurchaseOrderSys.Models
 {
+    /// <summary>
+    /// 登入用
+    /// </summary>
+    public class LoginViewModels
+    {
+        /// <summary>
+        /// 帳號
+        /// </summary>
+        [Display(Name = "Username", ResourceType = typeof(App_GlobalResources.Resource))]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(App_GlobalResources.Resource))]
+        public string Username { get; set; }
+        /// <summary>
+        /// 密碼
+        /// </summary>
+        [Display(Name = "Password", ResourceType = typeof(App_GlobalResources.Resource))]
+        [MinLength(3, ErrorMessageResourceName = "MinLength", ErrorMessageResourceType = typeof(App_GlobalResources.Resource))]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(App_GlobalResources.Resource))]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+    public class MenuAuth
+    {
+        public int MenuID { get; set; }
+        public List<string> AuthList { get; set; }
+    }
     public class CMVM
     {
         /// <summary>
