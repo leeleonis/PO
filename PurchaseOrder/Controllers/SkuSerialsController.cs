d68245e9ff284b3dd90720db5116ec1c5d3cecf3
leo <leoli.qd@hotmail.com>
2019-01-02 17:52:38 +0800

    更新合併

---
+++ PurchaseOrder/Controllers/SkuSerialsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PurchaseOrderSys.Controllers
+{
+    public class SkuSerialsController : BaseController
+    {
+        // GET: SkuSerials
+        public ActionResult Index()
+        {
+            var PurchaseSKU = db.PurchaseSKU.Find(12);
+            return View(PurchaseSKU);
+        }
+    }
+}
